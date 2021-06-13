f9bce58a6aa997e144cdfd911820b37ba0ecb018
Mike Furlender <furlender@mac.local>
2021-06-13 16:28:15 -0400

    initial page bugfix

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,11 +4,10 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
-      { path: 'dashboard/:symbol', component: () => import('pages/Index.vue'), meta: { skeleton: true }, props: true},
+      { path: '', redirect: '/BTCUSDT' },
+      { path: '/:symbol', component: () => import('pages/Index.vue'), meta: { skeleton: true }, props: true},
     ]
   },
-  {path: '/test/:symbol', component: ()=> import('components/BinanceCandles.vue') , props: true },
 
   // Always leave this as last one,
   // but you can also remove it
